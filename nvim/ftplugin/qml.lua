3e02c57c0a6f774947bc925d4a6bf7c2a7c90f45
kossLAN <kosslan@kosslan.dev>
2025-02-01 00:39:11 -0500

    nvim/qml: add proper tab width

--- nvim/ftplugin/qml.lua
+++ nvim/ftplugin/qml.lua
@@ -2,26 +2,6 @@ if vim.fn.executable('qmlformat') ~= 1 then
   return
 end
 
--- Stupid conform doesn't support qmlformat, and I don't want to download a
--- whole new fucking plugin so uh yea this is it ig.
--- vim.api.nvim_create_autocmd('BufWritePre', {
---   pattern = '*.qml',
---   callback = function()
---     local bufnr = vim.api.nvim_get_current_buf()
---     local filename = vim.api.nvim_buf_get_name(bufnr)
---     local cmd = string.format('qmlformat -i "%s"', filename)
---
---     local output = vim.fn.system(cmd)
---     local error_code = vim.v.shell_error
---
---     if error_code ~= 0 then
---       vim.notify(
---         string.format('qmlformat failed (code %d):\nCommand: %s\nOutput: %s', error_code, cmd, output),
---         vim.log.levels.ERROR
---       )
---     end
---
---     vim.api.nvim_buf_set_option(bufnr, 'modified', false)
---     vim.cmd('edit!')
---   end,
--- })
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
