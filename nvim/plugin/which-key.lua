4ce90e54b161e199cfbe4a8ec5ca6c9e4ef3eed2
kossLAN <kosslan@kosslan.dev>
2025-04-02 15:15:57 -0400

    whichkey: fix group names

--- nvim/plugin/which-key.lua
+++ nvim/plugin/which-key.lua
@@ -1,14 +1,17 @@
-require('which-key').setup()
--- local whichkey = require('which-key')
---
--- whichkey.setup()
---
--- whichkey.add {
---   { '<leader>c', group = '[C]ode' },
---   { '<leader>d', group = '[D]ocument' },
---   { '<leader>r', group = '[R]ename' },
---   { '<leader>s', group = '[S]earch' },
---   { '<leader>w', group = '[W]orkspace' },
---   { '<leader>t', group = '[T]oggle' },
---   { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
--- }
+local wk = require('which-key')
+
+wk.setup {
+  -- preset = 'helix',
+}
+
+wk.add {
+  { '<leader>c', group = '[C]ode' },
+  { '<leader>d', group = '[D]ebugger/Document' },
+  { '<leader>r', group = '[R]ename' },
+  { '<leader>s', group = '[S]earch' },
+  { '<leader>w', group = '[W]orkspace' },
+  { '<leader>t', group = '[T]oggle' },
+  { '<leader>g', group = '[G]it' },
+  { '<leader>p', group = '[P]eek' },
+  { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+}
