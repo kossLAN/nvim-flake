e54bd1fe69378440d2a69bd508d0e733143f982a
kossLAN <kosslan@kosslan.dev>
2025-05-29 23:51:07 -0400

    overlay: add vim-visual-multi

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -68,6 +68,7 @@ with final.pkgs.lib; let
     # ^ navigation/editing enhancement plugins
     # Useful utilities
     nvim-unception # Prevent nested neovim sessions | nvim-unception
+    vim-visual-multi
     # ^ Useful utilities
     # libraries that other plugins depend on
     sqlite-lua
