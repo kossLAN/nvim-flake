45c8f78d1c036dc923d93ef6bf221959b6f90436
kossLAN <kosslan@kosslan.dev>
2025-11-29 23:16:35 -0500

    fix: make the binds close terminal instantly...

--- nvim/plugin/toggleterm.lua
+++ nvim/plugin/toggleterm.lua
@@ -7,10 +7,19 @@ require('toggleterm').setup {
 
 -- Allow for 10 terminal spawns mapped to NumKeys.
 for i = 0, 9 do
+  vim.api.nvim_set_keymap('n', '<C-' .. i .. '>', '<CMD>ToggleTerm ' .. i .. '<CR>', { noremap = true, silent = true })
+
   vim.api.nvim_set_keymap(
-    'n',
-    '<c-' .. i .. '>',
-    '<CMD>ToggleTerm ' .. i .. '<CR>',
+    'i',
+    '<C-' .. i .. '>',
+    '<Esc><CMD>ToggleTerm ' .. i .. '<CR>',
+    { noremap = true, silent = true }
+  )
+
+  vim.api.nvim_set_keymap(
+    't',
+    '<C-' .. i .. '>',
+    '<C-\\><C-n><CMD>ToggleTerm ' .. i .. '<CR>',
     { noremap = true, silent = true }
   )
 end
